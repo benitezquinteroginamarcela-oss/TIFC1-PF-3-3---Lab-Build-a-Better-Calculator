f3aadb288c273e6a3e16ccce9966a9a04a3b58f3
Marce <benitezquinteroginamarcela@gmail.com>
2025-11-14 15:11:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ def main():
 
 if __name__=="__main__":
   main()
+  def addmultiplenumbers(numbers):
+    response = sum(numbers)
+    return response
+resultado = addmultiplenumbers([1, 2, 3, 4,])
+print("La suma es: ")
+
+mul = bool
+def multiplymultiplenumbers(numbers):
+    response = mul(numbers)
+    return response
+resultado = multiplymultiplenumbers([1, 2, 3, 4, 5, 6, 7, 8])
